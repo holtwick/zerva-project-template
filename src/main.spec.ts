cfdd6898e41d74a017d4cf8aada804748e385c05
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-11-26 19:24:26 +0100

    More compact

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,29 +1,15 @@
 // (C)opyright 2021-07-15 Dirk Holtwick, holtwick.it. All rights reserved.
 
 import "cross-fetch/polyfill"
-import { Logger, LoggerNodeHandler, LogLevel } from "zeed"
-import { serveStop, serve } from "zerva"
-
-import { setupService } from "./service"
-
-Logger.setHandlers([
-  LoggerNodeHandler({
-    level: LogLevel.info,
-    filter: "*",
-    colors: true,
-    padding: 16,
-    nameBrackets: false,
-    levelHelper: false,
-  }),
-])
+import { serve, serveStop } from "zerva"
+import { setup } from "./main"
 
 const port = 8080
 const url = `http://localhost:${port}`
 
 describe("service", () => {
   beforeAll(async () => {
-    setupService()
-    await serve()
+    await serve(setup)
   })
 
   afterAll(async () => {
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,44 @@
-import {
-  Logger,
-  LoggerFileHandler,
-  LoggerNodeHandler,
-  LogLevel,
-  toPath,
-} from "zeed"
-import { serve } from "zerva"
-import { setupService } from "./service"
+import { Logger } from "zeed"
+import { on, serve, useHttp } from "zerva"
 
-Logger.setHandlers([
-  LoggerFileHandler(toPath("data/zerva.log"), {
-    level: LogLevel.debug,
-  }),
-  LoggerNodeHandler({
-    level: LogLevel.info,
-    filter: "*",
-    colors: true,
-    padding: 32,
-    nameBrackets: false,
-    levelHelper: false,
-  }),
-])
+const log = Logger("main")
 
-serve(setupService)
+export async function setup() {
+  useHttp({
+    port: 8080,
+  })
+
+  on("httpInit", ({ get }) => {
+    const started = new Date().toISOString()
+    log.info(`Service started at ${started}`)
+
+    // @ts-ignore
+    get("/", ({ req }) => {
+      return `Running since ${started}. Your IP is ${req.ip}.`
+    })
+  })
+
+  // Example for repeating tasks - Cron like
+
+  // https://www.npmjs.com/package/cron
+  // https://blog.abelotech.com/posts/nodejs-scheduler-cron-timeout-interval/#explanation-of-a-cron-expression
+
+  // const job = new CronJob(
+  //   // "*/10 * * * * 0-6", // each 10 secs
+  //   "0 0 * * 0-6", // midnight
+  //   function () {
+  //     log.info("Midnight update triggered", new Date())
+  //   },
+  //   null,
+  //   true,
+  //   "Europe/Berlin"
+  // )
+  //
+  // log.info(
+  //   `Next 10 scheduled jobs:\n${(job.nextDates(10) as any[])
+  //     .map((date: any) => date.toString())
+  //     .join("\n")}`
+  // )
+}
+
+serve(setup)
